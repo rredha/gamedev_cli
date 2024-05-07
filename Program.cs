2dd79c4581c877218277d5dd0b36d0632d0dde3a
ahred <ahred.mbrk@gmail.com>
2024-05-07 16:18:19 +0100

    Begin implementing state machine

--- Program.cs
+++ Program.cs
@@ -1,58 +1,110 @@
-﻿using System.IO.Pipes;
+﻿using System.ComponentModel.Design;
+using System.IO.Pipes;
 using System.Runtime.CompilerServices;
 using System.Text;
 using angrybird_logic.ConsoleInterface;
 using angrybird_logic.GAction;
 using angrybird_logic.Units;
+using angrybird_logic.StateMachine;
 
 var cli = new ConsoleInterface();
 
-cli.Print("Angry Bird CLI testing");
-cli.Print("Select Action");
+PromptWelcome(cli);
+var state = StateMachine.State.LevelInit;
+var lvl = 1;
+cli.Print("Level : " + lvl.ToString());
+var prisonnersUnits = new List<Prisonner>();
+var targetUnits = new List<Target>();
+var projectileUnits = new List<Projectile>();
+SpawnerProjectileMechanism(cli);
+SpawnerTargetsMechanism(cli);
+SpawnerPrisonnersMechanism(cli);
+PromptSelection();
 
+state = StateMachine.State.Pick;
+PlayerPicking();
 
-List<GameAction> selectableItems = new List<GameAction>();
-var pick  = new Pick
+void PlayerPicking()
 {
-    _cli = cli
-};
-var aim   = new Aim
-{
-    _cli = cli
-};
-var shoot = new Shoot
-{
-    _cli = cli
-};
+   /*
+    * List all the projectiles that are available.
+    * Set one of them as selected
+    * Change state
+    */
 
-pick.Name = "pick";
-aim.Name = "aim";
-shoot.Name = "shoot";
-
-selectableItems.Add(pick);
-selectableItems.Add(aim);
-selectableItems.Add(shoot);
-
-foreach (var gameAction in selectableItems)
-{
-    gameAction.IsSelectable = true;
-    gameAction.PrintSelectableGameAction(gameAction.Name);
+   if (projectileUnits.Count != 0)
+   {
+       cli.Print(projectileUnits.Count + " Projectiles Left");
+       // error here, for some reason...
+       if (cli.UserInput != "sel") return;
+       // lets say that its picked and removed it from the list
+       projectileUnits.RemoveAt(projectileUnits.Count);
+       state = StateMachine.State.Aim;
+       PlayerAiming();
+   }
+   else
+   {
+       state = StateMachine.State.Lost;
+       PlayerLost();
+   }
 }
 
-var myUserInput = cli.UserInput;
-
-GameAction selectedGameAction; 
-GameAction GetCurrentInput(string input) => input switch
+void PlayerAiming()
 {
-    "pick" => selectedGameAction = pick,
-    "aim" => selectedGameAction = aim,
-    "shoot" => selectedGameAction = shoot,
-    _ => throw new ArgumentOutOfRangeException("not valid"),
-};
+    cli.Print("I'm Freaking Aiming at something...");
+}
 
-selectedGameAction = GetCurrentInput(myUserInput);
-selectedGameAction.PrintSelectedGameAction(selectedGameAction.Name);
-selectedGameAction.GameActionDoes();
+void PlayerLost()
+{
+    cli.Print("Shoot i lost again...");
+}
+
+void PromptSelection()
+{
+    List<GameAction> selectableItems = new List<GameAction>();
+    var pick  = new Pick
+    {
+        _cli = cli
+    };
+    var aim   = new Aim
+    {
+        _cli = cli
+    };
+    var shoot = new Shoot
+    {
+        _cli = cli
+    };
+
+    pick.Name = "pick";
+    aim.Name = "aim";
+    shoot.Name = "shoot";
+
+    selectableItems.Add(pick);
+    selectableItems.Add(aim);
+    selectableItems.Add(shoot);
+    // Sets all the options to be selectable...
+    // which is not always the case.
+    /*
+    foreach (var gameAction in selectableItems)
+    {
+        gameAction.IsSelectable = true;
+        gameAction.PrintSelectableGameAction(gameAction.Name);
+    }
+    */
+    selectableItems[0].IsSelectable = true;
+    selectableItems[1].IsSelectable = false;
+    selectableItems[2].IsSelectable = false;
+    
+    // print selectable game actions
+    foreach (var gameAction in selectableItems)
+    {
+        if (gameAction.IsSelectable)
+        {
+            gameAction.PrintSelectableGameAction(gameAction.Name);
+        }
+    }
+    GetSelection(cli, pick, aim, shoot);
+}
 
 /*
 bird.IsDoingWhileShooting();
@@ -65,20 +117,84 @@ pig.IsDoingWhenHit();
 prisoner.WhatAreYouDoing();
 */
 
-var projectileUnits = new List<Projectile>();
-SpawnProjectiles(3);
-cli.Print(projectileUnits.Count + " Projectiles");
+//SpawnerMechanism(cli);
+return;
 
-static T Spawn<T>() where T : new()
+void PromptWelcome(ConsoleInterface consoleInterface)
 {
-    return new T();
+    consoleInterface.Print("Angry Bird CLI testing");
+    consoleInterface.Print("Select Action");
 }
 
-void SpawnProjectiles(int num)
+void SpawnerProjectileMechanism(ConsoleInterface cli1)
 {
-    for (var i = 0; i < num; i++)
+    SpawnProjectiles(3);
+    cli1.Print(projectileUnits.Count + " Projectiles");
+
+    static T Spawn<T>() where T : new()
     {
-        projectileUnits.Add(Spawn<Bird>()); 
+        return new T();
+    }
+
+    void SpawnProjectiles(int num)
+    {
+        for (var i = 0; i < num; i++)
+        {
+            projectileUnits.Add(Spawn<Bird>()); 
+        }
     }
 }
-return;
+void SpawnerTargetsMechanism(ConsoleInterface cli1)
+{
+    SpawnTargets(2);
+    cli1.Print(targetUnits.Count + " Walls");
+
+    static T Spawn<T>() where T : new()
+    {
+        return new T();
+    }
+
+    void SpawnTargets(int num)
+    {
+        for (var i = 0; i < num; i++)
+        {
+            targetUnits.Add(Spawn<Wall>()); 
+        }
+    }
+}
+void SpawnerPrisonnersMechanism(ConsoleInterface cli1)
+{
+    SpawnPrsionners(1);
+    cli1.Print(prisonnersUnits.Count + " Prisonner");
+
+    static T Spawn<T>() where T : new()
+    {
+        return new T();
+    }
+
+    void SpawnPrsionners(int num)
+    {
+        for (var i = 0; i < num; i++)
+        {
+            prisonnersUnits.Add(Spawn<Prisonner>()); 
+        }
+    }
+}
+
+void GetSelection(ConsoleInterface consoleInterface, Pick pick1, Aim aim1, Shoot shoot1)
+{
+    var myUserInput = consoleInterface.UserInput;
+
+    GameAction selectedGameAction; 
+    GameAction GetCurrentInput(string input) => input switch
+    {
+        "pick" => selectedGameAction = pick1,
+        "aim" => selectedGameAction = aim1,
+        "shoot" => selectedGameAction = shoot1,
+        _ => throw new ArgumentOutOfRangeException("not valid"),
+    };
+
+    selectedGameAction = GetCurrentInput(myUserInput);
+    selectedGameAction.PrintSelectedGameAction(selectedGameAction.Name);
+    selectedGameAction.GameActionDoes();
+}
---
+++ StateMachine/StateMachine.cs
@@ -0,0 +1,14 @@
+namespace angrybird_logic.StateMachine;
+
+public class StateMachine
+{
+    public enum State 
+    {
+        LevelInit,
+        Pick,
+        Aim,
+        Shoot,
+        Result,
+        Lost,
+    }   
+}
